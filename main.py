96e0cf295f8684e760c251edc11bc8c1ddcffd21
DixonScott <159591477+DixonScott@users.noreply.github.com>
2025-08-15 15:38:42 +0100

    Add `simulate_battery.py`

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import pandas as pd
+
 from battery_scheduler.prepare_data import prepare_data
+from battery_scheduler.simulate_battery import simulate_battery
 
 
 def main():
     df = prepare_data()
+    df = simulate_battery(df)
+    pd.set_option('display.max_columns', None)
     print(df.head(10))
 
 
